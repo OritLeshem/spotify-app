1c975b1f6ed0f947ed66c94e32569bddcec4ee32
OritLeshem <azmono@gmail.com>
2023-03-22 10:39:42 -0400

    homepage

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,4 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import { PlaylistList } from '../cmps/playlist-list'
+import { PlaylistPreview } from '../cmps/playlist-preview'
+import { GreenBtn, LibrarySvg } from '../cmps/form'
+// import { Music } from '../cmps/music'
+import { playlistService } from '../services/playlist.service.local'
+import { youtubeService } from '../services/youtube.service'
+
 export function HomePage() {
-  return <>
-    hello from HomePage</>
+  const [pop, setPop] = useState()
+  const [latin, setLatin] = useState()
+  const [hiphop, setHiphop] = useState()
+
+  useEffect(() => {
+
+    const latin = playlistService.latin()
+    // console.log("latin", latin.songs)
+    setLatin(latin)
+    const hiphop = playlistService.hiphop()
+    // console.log("hiphop", hiphop.songs)
+    setHiphop(hiphop)
+    const pop = playlistService.pop()
+    // console.log("pop", pop.songs)
+    setPop(pop)
+  }, [])
+
+  youtubeService.getVideoResults("pink")
+    .then(res => {
+      // console.log('res:', res)
+    })
+
+  if (!pop) return
+  return <section className="main-page home-page">
+    {/* <Music /> */}
+    <h2>POP</h2>
+    <PlaylistList playlists={pop.songs} />
+    <h2>HIPHOP</h2>
+    <PlaylistList playlists={hiphop.songs} />
+    <h2>LATIN</h2>
+    <PlaylistList playlists={latin.songs} />
+  </section >
 }
