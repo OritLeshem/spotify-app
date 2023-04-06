15bbece07a637f96f58a8a51b36ddd00426a4856
OritLeshem <azmono@gmail.com>
2023-04-06 15:43:51 -0400

    deploy

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb+srv://admin:admin@cluster0.pdpx5xb.mongodb.net/?retryWrites=true&w=majority',
-  dbName: 'finderr_db'
+  dbURL: 'mongodb+srv://admin:1234@cluster0.alkbeng.mongodb.net/?retryWrites=true&w=majority',
+  dbName: 'spotify'
 }
